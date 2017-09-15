1a815f89d6aeea6f5d97ef79aa00ec0325cc4617
Eugene Oparyshev <eop@sitecore.net>
2017-09-15 11:06:52 +0300

    github patch

---
+++ src/Sitecore.Support.96652/Pipelines/HttpRequest/ItemVersionResolver.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Support.Pipelines.HttpRequest
+{
+    using Sitecore;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.HttpRequest;
+    using System;
+    using System.Linq;
+    public class ItemVersionResolver : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (Context.Item != null && !Context.Item.Versions.GetVersionNumbers().Contains(Context.Item.Version))
+            {
+                Context.Item = null;
+            }
+        }
+    }
+}
